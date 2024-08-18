7d5bc3d68c5cf5483949d9936a2077ad3c413f31
Chad Wright <justdeejays@gmail.com>
2024-08-18 11:09:32 +0200

    feat: add terms and conditions page

---
+++ app/terms-and-conditions/page.js
@@ -0,0 +1,270 @@
+const TermsAndConditions = () => {
+  return (
+    <main
+      className="py-20 desktopSmall:pt-40 desktopSmall:pb-20 max-w-[1550px] 
+    mx-auto"
+    >
+      <div className="mx-5 desktopSmall:mx-20">
+        <h1 className="mb-4">Xorbtec (Pty) Ltd Terms and Conditions for use</h1>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">Acceptance of Terms</h2>
+          <p className="mb-2">
+            Xorbtec (Pty) Ltd permits the use of this Website subject to these
+            terms and conditions ("the Terms and Conditions"). By using this
+            Website in any way, you shall be deemed to have accepted all the
+            Terms and Conditions unconditionally. You must not use this Website
+            if you do not agree to the Terms and Conditions. Your use of any
+            services offered by us will also be subject to applicable provisions
+            of the Terms and Conditions. In the event of any conflict between
+            the Terms and Conditions and any other conditions stipulated
+            elsewhere, the Terms and Conditions as contained herein will
+            prevail.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">Use of the Website</h2>
+          <p className="mb-2">
+            The contents of this Website, including any content, information,
+            software, icons, text, links, graphics, lay-outs, images, sound
+            clips, trade names, logos, trademarks and service marks are
+            protected by law, including but not limited to copyright and trade
+            mark law, and are owned by or licensed to Xorbtec (Pty) Ltd. No
+            license to or right in any of such contents is granted to or
+            conferred upon you. Any unauthorized use, distribution or
+            reproduction of the said contents is prohibited.
+          </p>
+          <p className="mb-2">
+            By entering this Website, you agree that you will not use any
+            device, software or other instrumentality to interfere or attempt to
+            interfere with the proper working of the Website and that you will
+            not take any action that imposes an unreasonable or
+            disproportionately large load on our infrastructure. In addition,
+            you agree that you will not use any robot, spider, other automatic
+            device, or manual process to monitor or copy the pages of the
+            Website or the content contained herein, without the prior written
+            consent from an authorized Xorbtec (Pty) Ltd representative (such
+            consent is deemed given for standard search engine technology
+            employed by Internet search websites to direct Internet users to
+            this Website). You may not use the Website to distribute material
+            which is defamatory, offensive, and unlawful or contains hate
+            speech.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">Disclaimer</h2>
+          <p className="mb-2">
+            While Xorbtec (Pty) Ltd takes reasonable measures to ensure that the
+            contents of this Website are accurate and complete, Xorbtec (Pty)
+            Ltd makes no representations or warranties, whether express or
+            implied, as to the quality, timeliness, operation, integrity,
+            availability or functionality of this Website or as to the accuracy,
+            completeness or reliability of any information on this Website.
+            Xorbtec (Pty) Ltd reserves the right to make changes, corrections
+            and/or improvements to the information and to the products and
+            programs described in such information, at any time without notice.
+          </p>
+          <p className="mb-2">
+            Xorbtec (Pty) Ltd will use reasonable endeavours to maintain the
+            availability of the Website, except during scheduled maintenance
+            periods, and reserves the right to discontinue providing the Website
+            or the service or any part thereof with or without notice to you.
+            Any person who accesses this Website or relies on this Website or on
+            the information contained in this Website does so at his or her own
+            risk. In addition to the disclaimers contained elsewhere in these
+            Terms and Conditions, Xorbtec (Pty) Ltd also makes no warranty or
+            representation, whether express or implied, that the information or
+            files available on this Website are free of viruses, spyware,
+            malware, trojans, destructive materials or any other data or code
+            which is able to corrupt, destroy, compromise or jeopardise the
+            operation, stability, security functionality or content of your
+            computer system, computer network, hardware or software in any way.
+            Xorbtec (Pty) Ltd does not accept any responsibility for any errors
+            or omissions on this Website.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">Privacy Notice</h2>
+          <p className="mb-2">
+            Xorbtec (Pty) Ltd has a strong commitment to providing excellent
+            service to all of our customers and visitors of this Website,
+            including respecting concerns about privacy. Xorbtec (Pty) Ltd will
+            explicitly ask when we need information that personally identifies
+            you or allows us to contact you ("personal information"). Generally
+            this information is requested when requesting a particular service
+            or product. You agree to provide accurate and current information,
+            and not to impersonate or misrepresent any person or entity or
+            falsely state or otherwise misrepresent your affiliation with anyone
+            or anything.
+          </p>
+          <p className="mb-2">
+            The purposes for which Xorbtec (Pty) Ltd will use your personal
+            information are as follows: to transact with you via the website or
+            email regarding purchase or service, to provide services to you via
+            our website; to inform you of new features, services, special offers
+            and products (provided you have consented to receiving such
+            marketing material); to enable us to process, validate and verify
+            requests for services and products; and for the purposes for which
+            you specifically provided the information; to improve your
+            experience on our website.
+          </p>
+          <p className="mb-2">
+            Xorbtec (Pty) Ltd shall be entitled to disclose personal information
+            if required to do so (a) to comply with applicable law or with legal
+            process served on Xorbtec (Pty) Ltd; (b) to protect and defend the
+            rights or property of Xorbtec (Pty) Ltd, and (c) for the purposes of
+            distributing same to various employees and/or third parties who
+            assist Xorbtec (Pty) Ltd in providing services to you and thus need
+            to know your personal information in order to render a proper and
+            efficient service to you. We will ensure that all such employees
+            and/or third party service providers having access to your personal
+            information are bound by appropriate and legally binding
+            confidentiality and non-use obligations in relation to your personal
+            information. You are aware that information and data is
+            automatically collected through the standard operation of the
+            Internet servers and through the use of "cookies." "Cookies" are
+            small text files a website can use to recognise repeat users,
+            facilitate the user's ongoing access to and use of the website and
+            allow a website to track usage behaviour and compile aggregate data
+            that will allow content improvements and targeted advertising.
+            Cookies are not programs that come onto your system and damage
+            files. Generally, cookies work by assigning a unique number to you
+            that has no meaning outside the assigning site. If you do not want
+            information collected through the use of cookies, there is a simple
+            procedure in most browsers that allows you to deny or accept the
+            cookie feature; however, you should note that cookies may be
+            necessary to provide you with certain features (e.g., customized
+            delivery of information) available on our Websites.
+          </p>
+          <p className="mb-2">
+            WHILST XORBTEC (PTY) LTD IS OF INTENT TO TAKE REASONABLE MEASURES TO
+            KEEP PERSONAL INFORMATION ABOUT YOU CONFIDENTIAL, IT SHALL HOWEVER
+            NOT BE LIABLE FOR ANY LOSS OR DAMAGE, HOWSOEVER ARISING, SUFFERED AS
+            A RESULT OF THE DISCLOSURE OF SUCH INFORMATION.
+          </p>
+          <p className="mb-2">Xorbtec (Pty) Ltd will:</p>
+          <ul className="list-disc list-inside pl-4 mb-2">
+            <li>treat your personal information as strictly confidential;</li>
+            <li>
+              take appropriate technical and organisational measures to ensure
+              that your personal information is kept secure and is protected
+              against unauthorised or unlawful processing, accidental loss,
+              destruction or damage, alteration, disclosure or access;
+            </li>
+            <li>
+              promptly notify you if we become aware of any unauthorised use,
+              disclosure or processing of your personal information;
+            </li>
+            <li>
+              provide you with reasonable evidence of our compliance with our
+              obligations under this notice on reasonable request; and
+            </li>
+            <li>
+              upon your request, promptly return or destroy any and all of your
+              personal information in our possession or control.
+            </li>
+          </ul>
+          <p className="mb-2">
+            We will not retain your personal information longer than the period
+            for which it was originally needed, unless we are required by law to
+            do so, or you consent to us retaining such information for a longer
+            period.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">
+            Linked Third-Party Website and Third-Party Content
+          </h2>
+          <p className="mb-2">
+            This Website may contain links or references to other websites
+            outside of our control, including those of advertisers. These Terms
+            and Conditions do not apply to those websites and Xorbtec (Pty) Ltd
+            is not responsible for the practices and/or privacy policies of
+            those sites or the cookies those sites use. In addition, because
+            Xorbtec (Pty) Ltd has no control over such external sites and
+            resources, you acknowledge and agree that Xorbtec (Pty) Ltd is not
+            responsible for the availability of such external sites or
+            resources, and does not endorse and is not responsible or liable for
+            any content, advertising, products or other materials on or
+            available from such sites or resources. You further acknowledge and
+            agree that Xorbtec (Pty) Ltd shall not be responsible or liable,
+            directly or indirectly, for any damage or loss caused or alleged to
+            be caused by or in connection with use of or reliance on any such
+            content, goods or services available on or through any such site or
+            resource.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">
+            Permission for Hyperlinks, Deep Linking, Crawlers and Metatags
+          </h2>
+          <p className="mb-2">
+            No user, business or website may link to any page on this site
+            without the prior written permission of Xorbtec (Pty) Ltd. Such
+            permission could be obtained from the person referred to in clause 1
+            hereof. Hyperlinks provided on this site to non-Xorbtec (Pty) Ltd
+            sites are provided as is and Xorbtec (Pty) Ltd does not necessarily
+            agree with, edit or sponsor the content on such web pages. No
+            person, business or website may frame this site or any of the pages
+            on this site in any way whatsoever. No person, business or website
+            may use any technology to search and gain any information from this
+            site without the prior written permission of Xorbtec (Pty) Ltd. Such
+            permission could be obtained from the person referred to in clause 1
+            hereof.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">Security</h2>
+          <p className="mb-2">
+            Any person that delivers or attempts to deliver any damaging code to
+            this site or attempts to gain unauthorized access to any page on
+            this site shall be prosecuted and civil damages shall be claimed in
+            the event that Xorbtec (Pty) Ltd suffers any damage or loss. You
+            agree and warrant that your log-in name and password shall be used
+            for your personal use only and not be disclosed to any third party.
+            You allow Xorbtec (Pty) Ltd to take all reasonable steps to ensure
+            the integrity and security of the site and back-office applications.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">
+            Changes to the Terms and Conditions
+          </h2>
+          <p className="mb-2">
+            Xorbtec (Pty) Ltd may, in its sole discretion, change these Terms
+            and Conditions or any part thereof at any time with or without
+            notice. It is your responsibility to visit the Website periodically
+            to review the Terms and Conditions to which you are bound. Changes
+            to these Terms and Conditions will become effective upon such
+            changes being posted to this Website and Xorbtec (Pty) Ltd will use
+            reasonable efforts to notify you of such changes, which you agree
+            will be sufficient notice of the changes.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="mb-2 text-subheading">
+            Compliance with Section 43(1) of ECTA
+          </h2>
+          <p className="mb-2">
+            In compliance with section 43(1) of the Electronic Communications
+            and Transactions Act ("ECTA") 25 of 2002, the following information
+            is provided:
+          </p>
+          <ul className="list-disc list-inside pl-4 mb-2">
+            <li>
+              Full name and legal status of the website owner: Xorbtec (Pty) Ltd
+            </li>
+            <li>Registration number: 2018/356137/07</li>
+            <li>
+              Physical address: Xorbtec House, 123 Business Park, City, Country
+            </li>
+            <li>Postal address: P.O. Box 1234, City, Country</li>
+            <li>Telephone number: +27 (0) 123 456 7890</li>
+            <li>Email address: info@xorbtec.com</li>
+          </ul>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default TermsAndConditions;
